55cbc33a068e787ec79db877eaf749a5ee069bb0
Dragos <dragosct10@gmail.com>
2017-10-02 19:16:39 +0300

    working on pro

--- _site/assets/js/demo.js
+++ _site/assets/js/demo.js
@@ -13,6 +13,16 @@ demo = {
         });
     },
 
+    checkFullPageBackgroundImage: function(){
+        $page = $('.full-page');
+        image_src = $page.data('image');
+
+        if(image_src !== undefined){
+            image_container = '<div class="full-page-background" style="background-image: url(' + image_src + ') "/>'
+            $page.append(image_container);
+        }
+    },
+
     initDateTimePicker: function() {
         if($(".datetimepicker").length != 0){
             $('.datetimepicker').datetimepicker({
@@ -60,6 +70,127 @@ demo = {
                }
             });
         };
+    },
+
+    initFullCalendar: function(){
+        $calendar = $('#fullCalendar');
+
+        today = new Date();
+        y = today.getFullYear();
+        m = today.getMonth();
+        d = today.getDate();
+
+        $calendar.fullCalendar({
+            viewRender: function(view, element) {
+                // We make sure that we activate the perfect scrollbar when the view isn't on Month
+                if (view.name != 'month'){
+                    $(element).find('.fc-scroller').perfectScrollbar();
+                }
+            },
+            header: {
+				left: 'title',
+				center: 'month,agendaWeek,agendaDay',
+				right: 'prev,next,today'
+			},
+			defaultDate: today,
+			selectable: true,
+			selectHelper: true,
+            views: {
+                month: { // name of view
+                    titleFormat: 'MMMM YYYY'
+                    // other view-specific options here
+                },
+                week: {
+                    titleFormat: " MMMM D YYYY"
+                },
+                day: {
+                    titleFormat: 'D MMM, YYYY'
+                }
+            },
+
+			select: function(start, end) {
+
+                // on select we show the Sweet Alert modal with an input
+				swal({
+    				title: 'Create an Event',
+    				html: '<div class="form-group">' +
+                            '<input class="form-control" placeholder="Event Title" id="input-field">' +
+                        '</div>',
+    				showCancelButton: true,
+                    confirmButtonClass: 'btn btn-success',
+                    cancelButtonClass: 'btn btn-danger',
+                    buttonsStyling: false
+                }).then(function(result) {
+
+                    var eventData;
+                    event_title = $('#input-field').val();
+
+                    if (event_title) {
+    					eventData = {
+    						title: event_title,
+    						start: start,
+    						end: end
+    					};
+    					$calendar.fullCalendar('renderEvent', eventData, true); // stick? = true
+    				}
+
+    				$calendar.fullCalendar('unselect');
+
+                });
+			},
+			editable: true,
+			eventLimit: true, // allow "more" link when too many events
+
+
+            // color classes: [ event-blue | event-azure | event-green | event-orange | event-red ]
+            events: [
+				{
+					title: 'All Day Event',
+					start: new Date(y, m, 1),
+                    className: 'event-default'
+				},
+				{
+					title: 'Meeting',
+					start: new Date(y, m, d-1, 10, 30),
+					allDay: false,
+					className: 'event-green'
+				},
+				{
+					title: 'Lunch',
+					start: new Date(y, m, d+7, 12, 0),
+					end: new Date(y, m, d+7, 14, 0),
+					allDay: false,
+					className: 'event-red'
+				},
+				{
+					title: 'Nud-pro Launch',
+					start: new Date(y, m, d-2, 12, 0),
+					allDay: true,
+					className: 'event-azure'
+				},
+				{
+					title: 'Birthday Party',
+					start: new Date(y, m, d+1, 19, 0),
+					end: new Date(y, m, d+1, 22, 30),
+					allDay: false,
+                    className: 'event-azure'
+				},
+				{
+					title: 'Click for Creative Tim',
+					start: new Date(y, m, 21),
+					end: new Date(y, m, 22),
+					url: 'http://www.creative-tim.com/',
+					className: 'event-orange'
+				},
+				{
+					title: 'Click for Google',
+					start: new Date(y, m, 21),
+					end: new Date(y, m, 22),
+					url: 'http://www.creative-tim.com/',
+					className: 'event-orange'
+				}
+			]
+		});
     },
 
     initDocumentationCharts: function(){
@@ -612,6 +743,45 @@ demo = {
             }
         });
     },
+
+    initVectorMap: function(){
+         var mapData = {
+                "AU": 760,
+                "BR": 550,
+                "CA": 120,
+                "DE": 1300,
+                "FR": 540,
+                "GB": 690,
+                "GE": 200,
+                "IN": 200,
+                "RO": 600,
+                "RU": 300,
+                "US": 2920,
+            };
+
+            $('#worldMap').vectorMap({
+                map: 'world_mill_en',
+                backgroundColor: "transparent",
+                zoomOnScroll: false,
+                regionStyle: {
+                    initial: {
+                        fill: '#e4e4e4',
+                        "fill-opacity": 0.9,
+                        stroke: 'none',
+                        "stroke-width": 0,
+                        "stroke-opacity": 0
+                    }
+                },
+
+                series: {
+                    regions: [{
+                        values: mapData,
+                        scale: ["#AAAAAA","#444444"],
+                        normalizeFunction: 'polynomial'
+                    }]
+                },
+            });
+	},
 
 
     initGoogleMaps: function(){
